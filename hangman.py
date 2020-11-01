3f1e237f9585e4fbd3f5525ddd215dd63e506c97
Raman <profazpcb@gmail.com>
2020-11-01 13:04:15 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -9,9 +9,25 @@ words = ["jumbo", "avengence", "importance", "desktop", "python", "premier", "pr
 value = randint(0, 9)
 print(words[value])
 
+secret = words[value]
+
 guess = ''
 
 for x in words[value]:
-    guess +=
+    guess += '-'
 
 print(guess)
+while(True):
+    letter = input("Guess a letter?")
+
+    new_guess = ''
+    for x in range(len(secret)):
+        if (secret[x] == letter):
+            new_guess += letter
+        elif (secret[x] == guess[x]):
+            new_guess += secret[x]
+        else:
+            new_guess += '-'
+    guess = new_guess
+
+    print(guess)
